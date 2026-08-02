f5edf1a43ee5a7acc61ba954b1c15a47d04c430f
jossy.rasoloharijaona <jossy.rasoloharijaona@openclassrooms.com>
2026-08-02 13:03:36 +0200

    chore(deps): upgrade Symfony framework from 6.4 to 7.4
    - Convert @Route annotations to PHP 8 attributes (Routing\Attribute\Route)
    - Replace removed getDoctrine() with ManagerRegistry injection in admin controllers
    - Remove dead imports and redundant config/routes/annotations.yaml
    - routes.yaml: type annotation -> attribute
    - framework.yaml: add handle_all_throwables: true
    - dev routing: switch profiler/framework XML resources to PHP
    - doctrine.yaml: set controller_resolver.auto_mapping: false
    - web_profiler.yaml: enable collect_serializer_data
    - Bump symfony/* constraints 6.4.* -> 7.4.*, php >=8.2
    - Apply Flex recipes (csrf, property_info, reference.php); drop enable_authenticator_manager
    - Result: Symfony 7.4.15, no deprecations, schema in sync

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use App\Entity\Media;
 use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
@@ -15,17 +15,13 @@ class HomeController extends AbstractController
     {
     }
 
-    /**
-     * @Route("/", name="home")
-     */
+    #[Route('/', name: 'home')]
     public function home()
     {
         return $this->render('front/home.html.twig');
     }
 
-    /**
-     * @Route("/guests", name="guests")
-     */
+    #[Route('/guests', name: 'guests')]
     public function guests()
     {
         $guests = $this->managerRegistry->getRepository(User::class)->findBy(['admin' => false]);
@@ -34,9 +30,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/guest/{id}", name="guest")
-     */
+    #[Route('/guest/{id}', name: 'guest')]
     public function guest(int $id)
     {
         $guest = $this->managerRegistry->getRepository(User::class)->find($id);
@@ -45,9 +39,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/portfolio/{id}", name="portfolio")
-     */
+    #[Route('/portfolio/{id}', name: 'portfolio')]
     public function portfolio(?int $id = null)
     {
         $albums = $this->managerRegistry->getRepository(Album::class)->findAll();
@@ -64,9 +56,7 @@ class HomeController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/about", name="about")
-     */
+    #[Route('/about', name: 'about')]
     public function about()
     {
         return $this->render('front/about.html.twig');
